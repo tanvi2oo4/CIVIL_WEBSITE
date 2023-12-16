0272701403dee33e24d84371e84d99965cf6cd33
tanvi2oo4 <ce22b035@iittp.ac.in>
2023-12-16 11:45:39 +0530

    Contact,Lab Webpage

---
+++ app.js
@@ -0,0 +1,30 @@
+const map = L.map('map').setView([13.7018,79.5946], 15);
+
+	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+	const marker = L.marker([51.5, -0.09]).addTo(map)
+		.bindPopup('<b>IIT TIRUPTI').openPopup();
+
+	const circle = L.circle([51.508, -0.11], {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5,
+		radius: 500
+	}).addTo(map).bindPopup('IIT Tirupati Region');
+
+	
+
+
+	
+	function onMapClick(e) {
+		popup
+			.setLatLng(e.latlng)
+			.setContent(`You clicked the map at ${e.latlng.toString()}`)
+			.openOn(map);
+	}
+
+	map.on('click', onMapClick);
+
